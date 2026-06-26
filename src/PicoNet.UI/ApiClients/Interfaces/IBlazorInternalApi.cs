6bf95f9e99f3ac5e99f301241b115bd017535b21
SudoScraTchY <SainScraTchY@gmail.com>
2026-06-26 09:50:27 +0330

    chore: add new package references for authentication and icons

--- src/PicoNet.UI/ApiClients/Interfaces/IBlazorInternalApi.cs
+++
@@ -1,9 +0,0 @@
-﻿using ErrorOr;
-using PicoNet.Contracts.DTOs.Requests.Auth;
-
-namespace PicoNet.UI.ApiClients.Interfaces;
-
-public interface IBlazorInternalApi
-{
-    Task<ErrorOr<HttpResponseMessage>> Login(LoginRequest model);
-}
--- src/PicoNet.UI/Services/CurrentUserTokenAccessor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PicoNet.UI.Services;
-
-public sealed class CurrentUserTokenAccessor
-{
-    public string? AccessToken { get; set; }
-    public string? RefreshToken { get; set; }
-    public DateTime? AccessTokenExpiresAt { get; set; }
-    public DateTime? RefreshTokenExpiresAt { get; set; }
-}
